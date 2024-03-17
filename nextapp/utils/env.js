eb9b2d2f3c53880701b09a3eb88a62004fba52a4
handtrixxx <niklas.stephan@gmail.com>
2024-03-17 18:42:52 +0000

    deploy updates

--- nextapp/utils/env.js
+++ nextapp/utils/env.js
@@ -1,19 +1,5 @@
 // env.js
 
-// A function to read and validate the API_URL environment variable
-/*
-const url_backend = (() => {
-  const url_backend = process.env.BACKEND_URL;
-  if (!url_backend) {
-    throw new Error('The BACKEND_URL environment variable is not set');
-  }
-  if (!url_backend.startsWith('http')) {
-    throw new Error('The BACKEND_URL environment variable must start with http or https');
-  }
-  return url_backend;
-})();
-*/
-
 function validateUrl(url) {
   if (!url) {
     throw new Error('The URL '+url+' is not set in the environment variables');
